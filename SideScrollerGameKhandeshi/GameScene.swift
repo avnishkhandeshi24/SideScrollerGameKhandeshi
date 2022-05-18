afa53ae38d5b7285c5fbb6021680dfb34eb4a145
Avnish K <avnish.khandeshi24@d155.org>
2022-05-18 09:45:25 -0500

    Side scroller game - init commit

--- SideScrollerGameKhandeshi/GameScene.swift
+++ SideScrollerGameKhandeshi/GameScene.swift
@@ -8,16 +8,52 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    
+    var gameTimer = 1000
+    //var timer = Timer()
+    
+    var onGround = true
+    
+    var startingDxValue : CGFloat?
+    
+    var newDxValue : CGFloat?
+    
+    // Ball
+    var ball : SKSpriteNode!
+    
+    
+    // Camera
+    let cam = SKCameraNode()
+    
+    
+    var finalScoreLabel : SKLabelNode!
+    
+    //var entities = [GKEntity]()
+    //var graphs = [String : GKGraph]()
     
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func sceneDidLoad() {
+        
+        physicsWorld.contactDelegate = self
+        
+        // Connects ball variable to scene ball
+        ball = (self.childNode(withName: "ball") as! SKSpriteNode)
+        
+        
+        // Connects the cam variable to the current cam
+        self.camera = cam
+        
+        finalScoreLabel = self.childNode(withName: "finalScore") as? SKLabelNode
+
+        // Gets starting dx value
+        startingDxValue = ball.physicsBody?.velocity.dx
+        newDxValue = startingDxValue
+        
 
         self.lastUpdateTime = 0
         
@@ -39,10 +75,13 @@ class GameScene: SKScene {
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
+            
+            
+            
         }
     }
     
-    
+    /*
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -106,4 +145,81 @@ class GameScene: SKScene {
         
         self.lastUpdateTime = currentTime
     }
+     */
+    
+    
+    // Continuously updates the camera
+    override func update(_ currentTime: TimeInterval) {
+        
+        // Sets the center of the camera to the center of the ball
+        cam.position = CGPoint(x: ball.position.x, y: 0.0)
+        
+    }
+    
+    
+    func jumpBall() {
+        if (onGround == true) {
+            newDxValue! += 15
+            ball.physicsBody?.affectedByGravity = false
+            ball.physicsBody?.velocity = CGVector(dx: newDxValue!, dy: 750)
+            onGround = false
+            //ball.physicsBody?.velocity = CGVector(dx : 100.0, dy: 0)
+        }
+        else {
+            newDxValue! += 15
+            ball.physicsBody?.velocity = CGVector(dx: newDxValue!, dy: 0)
+            ball.physicsBody?.affectedByGravity = true
+            onGround = true
+        }
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.node?.name == "ball" && contact.bodyB.node?.name == "ground") {
+            onGround = true
+        }
+        else if (contact.bodyA.node?.name == "ball" && contact.bodyB.node?.name == "ceiling") {
+            ball.physicsBody?.velocity = CGVector(dx: newDxValue!, dy: 0)
+        }
+        if ((contact.bodyA.node?.name! == "ball" && contact.bodyB.node?.name! == "obstacle") || ( contact.bodyA.node?.name! == "obstacle" && contact.bodyB.node?.name! == "ball")) {
+            print("ok")
+            reset()
+        }
+        else if (contact.bodyA.node?.name == "ball" && contact.bodyB.node?.name == "finishLine" || contact.bodyB.node?.name == "ball" && contact.bodyA.node?.name == "finishLine") {
+            let gameSceneTemp = GameScene(fileNamed: "GameScene")!
+            self.scene?.view?.presentScene(gameSceneTemp, transition: SKTransition.fade(withDuration: 6))
+            
+            let p = ball.physicsBody
+            ball.physicsBody = nil
+            ball.position = CGPoint(x: 1000, y: -2500)
+            ball.physicsBody = p
+            
+            ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            
+            ball.isHidden = true
+            
+            cam.position = CGPoint(x: 1000, y: -2500)
+            let finalScore = gameTimer
+            finalScoreLabel.text = "Score: \(finalScore)"
+            
+        }
+    }
+    func reset() {
+        newDxValue = startingDxValue
+        let p = ball.physicsBody
+        ball.physicsBody = nil
+        ball.position = CGPoint(x: -541.914, y: -217.379)
+        ball.physicsBody = p
+        gameTimer -= 10
+    }
+    
+   /* @objc func updateTimer() {
+        gameTimer += 1
+    }
+    
+    @objc func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameScene.updateTimer), userInfo: nil, repeats: true)
+    }*/
+
+    
 }
--- SideScrollerGameKhandeshi/GameViewController.swift
+++ SideScrollerGameKhandeshi/GameViewController.swift
@@ -11,8 +11,21 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    //var gameTimer = 1000
+    var timer = Timer()
+    
+    var play : GameScene!
+    
+    @IBOutlet weak var jumpButton: UIButton!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
@@ -21,9 +34,12 @@ class GameViewController: UIViewController {
             // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! GameScene? {
                 
+                // Connecting the storyboard (with the button) to the GameScene (with the ball)
+                play = sceneNode
+                
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+             //   sceneNode.entities = scene.entities
+               // sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
@@ -39,6 +55,7 @@ class GameViewController: UIViewController {
                 }
             }
         }
+        startTimer()
     }
 
     override var shouldAutorotate: Bool {
@@ -46,14 +63,28 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscapeRight
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    
+    @IBAction func jumpButtonAction(_ sender: UIButton) {
+        
+        play.jumpBall()
+        
+    }
+    
+    
+    @objc func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        play.gameTimer -= 1
+        timerLabel.text = "Score: \(play.gameTimer)"
+    }
+    
 }
